e151938e8e5407376ef89046afa312c516d64e4c
hamza202 <k8668975@gmail.com>
2019-10-22 20:59:57 +0300

    contact

--- menu.php
+++ menu.php
@@ -113,30 +113,37 @@
                     </ul>
                 </div>
             </li>
-<!--            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a-->
-<!--                        href="javascript:;" class="m-menu__link m-menu__toggle"><i-->
-<!--                            class="m-menu__link-icon  flaticon-user"></i><span-->
-<!--                            class="m-menu__link-text">ثوابت النظام</span><i-->
-<!--                            class="m-menu__ver-arrow la la-angle-right"></i></a>-->
-<!--                <div class="m-menu__submenu "><span class="m-menu__arrow"></span>-->
-<!--                    <ul class="m-menu__subnav">-->
-<!--                        <li class="m-menu__item " aria-haspopup="true">-->
-<!--                            <a href="#!"-->
-<!--                               class="m-menu__link "><i-->
-<!--                                        class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span-->
-<!--                                        class="m-menu__link-text">اضافة مدن وقرى</span>-->
-<!--                            </a>-->
-<!--                        </li>-->
-<!--                        <li class="m-menu__item " aria-haspopup="true">-->
-<!--                            <a href="#!"-->
-<!--                               class="m-menu__link "><i-->
-<!--                                        class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span-->
-<!--                                        class="m-menu__link-text">اسعار مدن وقرى</span>-->
-<!--                            </a>-->
-<!--                        </li>-->
-<!--                    </ul>-->
-<!--                </div>-->
-<!--            </li>-->
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+                <a href="contact.php" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon flaticon-chat-1">
+                    </i><span class="m-menu__link-text">رسائل الموقع</span>
+                </a>
+
+            </li>
+            <!--            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a-->
+            <!--                        href="javascript:;" class="m-menu__link m-menu__toggle"><i-->
+            <!--                            class="m-menu__link-icon  flaticon-user"></i><span-->
+            <!--                            class="m-menu__link-text">ثوابت النظام</span><i-->
+            <!--                            class="m-menu__ver-arrow la la-angle-right"></i></a>-->
+            <!--                <div class="m-menu__submenu "><span class="m-menu__arrow"></span>-->
+            <!--                    <ul class="m-menu__subnav">-->
+            <!--                        <li class="m-menu__item " aria-haspopup="true">-->
+            <!--                            <a href="#!"-->
+            <!--                               class="m-menu__link "><i-->
+            <!--                                        class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span-->
+            <!--                                        class="m-menu__link-text">اضافة مدن وقرى</span>-->
+            <!--                            </a>-->
+            <!--                        </li>-->
+            <!--                        <li class="m-menu__item " aria-haspopup="true">-->
+            <!--                            <a href="#!"-->
+            <!--                               class="m-menu__link "><i-->
+            <!--                                        class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span-->
+            <!--                                        class="m-menu__link-text">اسعار مدن وقرى</span>-->
+            <!--                            </a>-->
+            <!--                        </li>-->
+            <!--                    </ul>-->
+            <!--                </div>-->
+            <!--            </li>-->
         </ul>
     </div>
 
