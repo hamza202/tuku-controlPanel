56a5940c7514b8ea95b179d619a7b2f8d2987020
hamza202 <k8668975@gmail.com>
2019-10-30 17:49:32 +0200

    last

--- menu.php
+++ menu.php
@@ -118,7 +118,48 @@
                     <i class="m-menu__link-icon flaticon-chat-1">
                     </i><span class="m-menu__link-text">رسائل الموقع</span>
                 </a>
+            </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover">
+                <a href="web-control.php" class="m-menu__link m-menu__toggle">
+                    <i class="m-menu__link-icon fas fa-sitemap">
+                    </i><span class="m-menu__link-text">التحكم بالموقع</span>
+                </a>
 
+            </li>
+            <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a
+                        href="javascript:;" class="m-menu__link m-menu__toggle"><i
+                            class="m-menu__link-icon  far fa-copy"></i><span
+                            class="m-menu__link-text">صفحات الموقع</span><i
+                            class="m-menu__ver-arrow la la-angle-right"></i></a>
+                <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+                    <ul class="m-menu__subnav">
+                        <li class="m-menu__item " aria-haspopup="true">
+                            <a href="about-us.php"
+                               class="m-menu__link "><i
+                                        class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span
+                                        class="m-menu__link-text">من نحن</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true">
+                            <a href="work-method.php" class="m-menu__link ">
+                                <i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i>
+                                <span class="m-menu__link-text">آلية العمل</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true">
+                            <a href="conditions.php" class="m-menu__link ">
+                                <i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i>
+                                <span class="m-menu__link-text">شروط الإستخدام</span>
+                            </a>
+                        </li>
+                        <li class="m-menu__item " aria-haspopup="true">
+                            <a href="privacy.php" class="m-menu__link ">
+                                <i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i>
+                                <span class="m-menu__link-text">سياسة الخصوصية</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
             <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a
                         href="javascript:;" class="m-menu__link m-menu__toggle"><i
